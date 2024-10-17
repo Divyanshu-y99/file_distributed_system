afcb13520098f31c1a334e7b484352f9c46ae8b4
divyanshu293 <ydivyanshu1999@gmail.com>
2024-10-17 21:43:29 +0530

    message store

--- store.go
+++ store.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
-type PathTransformFunc func(string) string
+func CASPathTransformFun(key string) PathKey {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blocksize := 5
+	sliceLen := len(hashStr) / blocksize
+
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blocksize, (i*blocksize)+blocksize
+		paths[i] = hashStr[from:to]
+	}
+
+	return PathKey{
+		PathName: strings.Join(paths, "/"),
+		Filename: hashStr,
+	}
+
+}
+
+type PathTransformFunc func(string) PathKey
+
+type PathKey struct {
+	PathName string
+	Filename string
+}
+
+func (p PathKey) FullPath() string {
+	return fmt.Sprintf("%s/%s", p.PathName, p.Filename)
+}
 
 type StoreOpts struct {
 	PathTransformFunc PathTransformFunc
@@ -26,28 +61,46 @@ func NewStore(opts StoreOpts) *Store {
 	}
 }
 
-func (s *Store) writeStream(key string, r io.Reader) error {
-	pathName := s.PathTransformFunc(key)
+func (s *Store) Read(key string) (io.Reader, error) {
+	f, err := s.readStream(key)
+	if err != nil {
+		return nil, err
+	}
 
-	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+	defer f.Close()
+
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, f)
+	return buf, err
+}
+
+func (s *Store) readStream(key string) (io.ReadCloser, error) {
+	PathKey := s.PathTransformFunc(key)
+	return os.Open(PathKey.FullPath())
+}
+
+func (s *Store) writeStream(key string, r io.Reader) error {
+	pathKey := s.PathTransformFunc(key)
+
+	if err := os.MkdirAll(pathKey.PathName, os.ModePerm); err != nil {
 		return err
 	}
 
-	filename := "somefilename"
-	pathAndFilename := pathName + "/" + filename
+	fullPath := pathKey.FullPath()
 
-	f, err := os.Create(pathAndFilename)
-
+	f, err := os.Create(fullPath)
 	if err != nil {
 		return err
 	}
+
+	// defer f.Close()
 
 	n, err := io.Copy(f, r)
 	if err != nil {
 		return err
 	}
 
-	log.Printf("written (%d) bytes to disk: %s", n, pathAndFilename)
+	log.Printf("written (%d) bytes to disk: %s", n, fullPath)
 
 	return nil
 }
--- store_test.go
+++ store_test.go
@@ -2,17 +2,51 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io"
+
 	"testing"
 )
 
+// func TestPathTransformFunc(t *testing.T) {
+// 	key := "momsbestpicture"
+// 	pathKey := CASPathTransformFun(key)
+// 	expectedOriginalKey := "6804429f74181a63c50c3d81d733a12f14a353ff"
+// 	expectedPathName := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
+
+// 	if pathKey.PathName != expectedPathName {
+// 		t.Error(t, "have %s want %s", pathKey.PathName, expectedPathName)
+// 	}
+
+// 	if pathKey.Filename != expectedPathName {
+// 		t.Error(t, "have %s want %s", pathKey.Filename, expectedOriginalKey)
+// 	}
+
+// }
+
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFun,
 	}
 	s := NewStore(opts)
+	key := "momsspecials"
 
-	data := bytes.NewReader([]byte("some jpg bytes"))
-	if err := s.writeStream("myspecialpicture", data); err != nil {
+	data := []byte("some jpg bytes")
+
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
 		t.Error(err)
+	}
+
+	r, err := s.Read(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := io.ReadAll(r)
+
+	fmt.Println(string(b))
+
+	if string(b) != string(data) {
+		t.Errorf("want %s have %s", data, b)
 	}
 }
